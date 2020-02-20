0f31a8f1774bf6d7f47b385e5ff75c7924f4da3f
azputra <ahmadzp102@gmail.com>
2020-02-20 23:19:53 +0700

    modified:   src/App.vue modified:   src/components/CreateName.vue ==================>>>>> fixed conflict

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,29 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <div class="container-fluidS">
-      <router-view/>
-=======
-    <router-view/>
+    <router-view />
     <div class="container-fluid">
-
-     <front />
->>>>>>> 0445f0a9050e957f1546c6e52a793fbf01025766
+      <front />
     </div>
   </div>
 </template>
 
 <script>
-import Front from "./views/Front"
+import Front from "./views/Front";
 export default {
-    components: {
-      Front
-    }
-}
+  components: {
+    Front
+  }
+};
 </script>
 
 <style lang="scss">
-@import './assets/scss/style.scss';
+@import "./assets/scss/style.scss";
 #app {
-<<<<<<< HEAD
   background-image: url("./assets/images/bg-home.jpg");
   background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    width: 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
   height: 97.5vh;
-=======
-  background-color: cadetblue;
-  height: 100vh;
->>>>>>> 0445f0a9050e957f1546c6e52a793fbf01025766
 }
-
 </style>
