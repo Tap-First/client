f729de3d0839e21f3d7c0b6f0f88657ea362a35b
mustaqinIshak <mustaqinishak@gmail.com>
2020-02-20 19:24:06 +0700

    create front page

--- src/components/CreateName.vue
+++ src/components/CreateName.vue
@@ -1,10 +1,30 @@
 <template>
-  
+    
+    <div class="row">
+        <div class="offset-md-2 col-md-8 p-3">
+            <form action="" v-on:submit.prevent="createName">
+                <div class="form-group">
+                    <label for="name">Input name :</label>
+                    <input id="name" type="text" v-model="name" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary">submit</button>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            name: null
+        }
+    },
+    method: {
+        createName: () => {
+            localStorage.setItem("name", name)
+        }
+    }
 }
 </script>
 
