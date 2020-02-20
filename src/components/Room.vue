dd33ae133629e90fc75ccbc19bb89e099619813d
azputra <ahmadzp102@gmail.com>
2020-02-21 02:26:34 +0700

    modified:   src/components/CreateRoom.vue         modified:   src/components/Room.vue ======================++> buat create room sama room

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,24 +1,25 @@
 <template>
-<div>
-  <create-room />
-  <div class="row room">
-    <div class="column">
-      <div class="card">
-        <img
-          src="https://cdnb.artstation.com/p/assets/images/images/012/193/905/original/maria-hager-titlescreen-retro-glitch.gif?1533552570"
-          style="max-width: 28vh;min-height: 28vh;border-radius: 100%;margin-top: 2%;"
-        />
-        <h1>Room 1</h1>
-        <button>JOIN</button>
+  <div>
+    <create-room @getRoom="findAllRooms" />
+    <div class="row room">
+      <div class="column" v-for="(room,i) in this.rooms.rooms" :key="i">
+        <div class="card">
+          <img
+            src="https://cdnb.artstation.com/p/assets/images/images/012/193/905/original/maria-hager-titlescreen-retro-glitch.gif?1533552570"
+            style="max-width: 28vh;min-height: 28vh;border-radius: 100%;margin-top: 2%;"
+          />
+          <h1>{{room.name}}</h1>
+          <button @click="goToIngame(room.id)">JOIN</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
 import socket from "@/config/socket.js";
-import CreateRoom from "../components/CreateRoom"
+import CreateRoom from "../components/CreateRoom";
+import axios from "axios";
 export default {
   name: "Room",
   data() {
@@ -28,6 +29,39 @@ export default {
   },
   components: {
     CreateRoom
+  },
+  methods: {
+    findAllRooms() {
+      axios({
+        url: "http://localhost:3000/rooms",
+        method: "get"
+      })
+        .then(({ data }) => {
+          this.rooms = data;
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
+    },
+    goToIngame(id) {
+      axios({
+        url: "http://localhost:3000/updateplayer",
+        method: "put",
+        data: {
+          id,
+          name: localStorage.getItem("name")
+        }
+      })
+        .then(({ data }) => {
+          this.$router.push("/ingame");
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
+    }
+  },
+  created() {
+    this.findAllRooms();
   }
 };
 </script>
@@ -35,7 +69,6 @@ export default {
 <style scoped>
 .room {
   font-family: Arial, Helvetica, sans-serif;
-  /* background-color: rgb(255, 0, 0); */
 }
 
 .column {
@@ -70,7 +103,7 @@ export default {
 }
 
 .card:hover {
-  box-shadow: 0 4px 8px 0 black;
+  box-shadow: 0 4px 8px 0 rgb(93, 93, 93);
 }
 
 .title {
@@ -78,14 +111,27 @@ export default {
   font-size: 18px;
 }
 button {
-  border: none;
-  padding: 8px;
+  box-shadow: 3px 4px 0px 0px #899599;
+  background: linear-gradient(to bottom, #ededed 5%, #bab1ba 100%);
+  background-color: #ededed;
+  border-radius: 15px;
+  border: 1px solid #d6bcd6;
   display: inline-block;
-  color: rgb(0, 0, 0);
   cursor: pointer;
-  width: 100%;
-  border-radius: 20px;
-  text-align: center;
+  color: #3a8a9e;
+  font-family: Arial;
+  font-size: 17px;
+  padding: 7px 25px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #e1e2ed;
   outline: none;
+}
+button:active {
+  position: relative;
+  top: 3px;
+}
+button:hover {
+  background: linear-gradient(to bottom, #bab1ba 5%, #ededed 100%);
+  background-color: #bab1ba;
 }
 </style>
